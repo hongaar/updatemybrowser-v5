c704b564e159a83921b6ced100c983bc24ac5b2a
Joram van den Boezem <joram@vandenboezem.nl>
2023-11-22 10:27:26 +0000

    chore: possible 5xx fix?

--- packages/web/src/components/Navigation/Client.tsx
+++ packages/web/src/components/Navigation/Client.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import NextTopLoader from "nextjs-toploader";
 import NProgress from "nprogress";
 import { useEffect } from "react";
 
 export function Client() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     NProgress.done();
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return <NextTopLoader color="#1095c1" showSpinner={false} />;
 }
