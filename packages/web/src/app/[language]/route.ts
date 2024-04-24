d8aed869727a40865445ac2233baf0a644aad45a
Joram van den Boezem <joram@vandenboezem.nl>
2024-04-24 11:10:49 +0200

    fix: add missing images and fix redirect to 404

--- packages/web/src/app/[language]/route.ts
+++ packages/web/src/app/[language]/route.ts
@@ -11,6 +11,19 @@ export async function GET(
   { params: { language } }: LanguageParams,
 ) {
   const url = request.nextUrl.clone();
+
+  if (url.pathname.length > 3) {
+    return new Response(
+      '<body>Sorry, the page you tried to visit does not exist (anymore). <a href="/">Return to the homepage</a></body>',
+      {
+        status: 404,
+        headers: {
+          "Content-Type": "text/html",
+        },
+      },
+    );
+  }
+
   url.pathname = `/${language}/check`;
 
   return NextResponse.redirect(url, 308);
